b215a4f792439cb043abd900a754d90b3b4d704c
Ignacio Gavilanes <77927772+IgnacioGavilanes@users.noreply.github.com>
2021-09-04 19:22:37 -0300

    Manipulación del DOM
    Grid dinámico de productos

--- JavaScript/proyectoFinal/js/data.js
+++ JavaScript/proyectoFinal/js/data.js
@@ -2,34 +2,58 @@
 THIS FILE CONTAINS ALL THE INFORMATION REGARDING THE PRODUCTS ON THE PROJECT. WILL MOVE TO JSON FILE EVENTUALLY 
 ----------------------------------------------------------------------------------------------------------*/
 
-
-
-const colors = 
-['green', 'black', 'white', 'yellow', 'pink', 'purple', 'blue', 'red', 'orange', 'burgundy', 'grey', 'beige']; 
-//INDEXES: 0 - green, 1 - black, 2 - white, 3 - yellow, 4 - pink, 5 - purple, 6 - blue, 7 - red, 8 - orange, 9 - burgundy, 10 - grey, 11 - beige
-
-//STILL NEED TO UPDATE SHOE PRICES AND FIND A SOLUTION FOR ONLY 1 COLOR SHOES. NEED TO FIX EMPTY STRINGS FOR SHOES WITH NO COLLAB VALUE
 const storeItems = [
-    //only 1 color
-    {id: 1, title: '327casablanca', brand: 'newbalance', collab: 'casablanca', type: 'low', colorPrimary: colors[2], colorSecondary: colors[0], stock: 3, price: 120},
-    {id: 2, title: 'adidasraf', brand: 'adidas', collab: 'rafsimons', type: 'low', colorPrimary: colors[3], colorSecondary: colors[1], stock: 2, price: 170},
-    {id: 3, title: 'balenciagasss', brand: 'balenciaga', collab: '', type: 'low', colorPrimary: colors[1], colorSecondary: colors[7], stock: 1,  price: 300},
-    {id: 4, title: 'balenciagatrack', brand: 'balenciaga', collab: '', type: 'low', colorPrimary: colors[8], colorSecondary: colors[6], stock: 2,  price: 375},
-    {id: 5, title: 'blazersacai', brand: 'nike', collab: 'sacai', type: 'high', colorPrimary: colors[3], colorSecondary: colors[6], stock: 2,  price: 140},
-    {id: 6, title: 'diorb23', brand: 'dior', collab: '', type: 'high', colorPrimary: colors[2], colorSecondary: colors[1], stock: 4,  price: 350},
-    {id: 7, title: 'nb990', brand: 'newbalance', collab: '', type: 'low', colorPrimary: colors[0], colorSecondary: colors[5], stock: 2,  price: 160},
-    {id: 8, title: 'nb2002', brand: 'newbalance', collab: '', type: 'low', colorPrimary: colors[8], colorSecondary: colors[1], stock: 3,  price: 125},
-    {id: 9, title: 'nikealphafly', brand: 'nike', collab: '', type: 'low', colorPrimary: colors[1], colorSecondary: colors[0], stock: 1,  price: 220},
-    //only 1 color
-    {id: 10, title: 'nikebb', brand: 'nike', collab: '', type: 'low', colorPrimary: colors[10], colorSecondary: colors[6], stock: 4,  price: 300},
-    {id: 11, title: 'offjordan1', brand: 'nike', collab: 'offwhite', type: 'high', colorPrimary: colors[2], colorSecondary: colors[7], stock: 3,  price: 190},
-    {id: 12, title: 'offlowdunk', brand: 'nike', collab: 'offwhite', type: 'low', colorPrimary: colors[0], colorSecondary: colors[8], stock: 3,  price: 180},
-    {id: 13, title: 'offnikezoom', brand: 'nike', collab: 'offwhite', type: 'low', colorPrimary: colors[4], colorSecondary: colors[5], stock: 1,  price: 260},
-    //only 1 color
-    {id: 14, title: 'offodsy2000', brand: 'offwhite', collab: '', type: 'low', colorPrimary: colors[6], colorSecondary: colors[2], stock: 2,  price: 275},
-    {id: 15, title: 'wafflesacai', brand: 'nike', collab: 'sacai', type: 'low', colorPrimary: colors[9], colorSecondary: colors[0], stock: 5,  price: 180},
-    //only 1 color
-    {id: 16, title: 'yeezy500', brand: 'adidas', collab: 'yeezy', type: 'low', colorPrimary: colors[11], colorSecondary: colors[0], stock: 4,  price: 200},
-    {id: 17, title: 'yeezy350', brand: 'adidas', collab: 'yeezy', type: 'low', colorPrimary: colors[1], colorSecondary: colors[2], stock: 2,  price: 220},
-    {id: 18, title: 'yeezy700', brand: 'adidas', collab: 'yeezy', type: 'low', colorPrimary: colors[6], colorSecondary: colors[2], stock: 3,  price: 250},
-];
+    {id: 1, title: "New Balance 327 x Casablanca", colorway: "Green Logo Colorway", brand: "newbalance", collab: "casablanca", 
+    type: "low", reviews: 22, colorPrimary: "white", colorSecondary: "green", stock: 3, price: 120, imgsrc: "img/327casablanca.svg"},
+
+    {id: 2, title: "Adidas Ozweego x Raf Simons", colorway: "Bright Yellow Night Navy Colorway", brand: "adidas", collab: "rafsimons", 
+    type: "low", reviews: 22, colorPrimary: "yellow", colorSecondary: "black", stock: 2, "price": 170, imgsrc: "img/adidasraf.svg"},
+
+    {id: 3, title: "Balenciaga Triple S", colorway: "Red / Black Colorway", brand: "balenciaga", collab: null,
+    type: "low", reviews: 67, colorPrimary: "black", colorSecondary: "red", stock: 1, price: 300, imgsrc: "img/balenciagasss.svg"},
+
+    {id: 4, title: "Balenciaga Track", colorway: "Orange / Blue Colorway", brand: "balenciaga", collab: null, 
+    type: "low", reviews: 44, colorPrimary: "orange", colorSecondary: "blue", stock: 2, price: 375, imgsrc: "img/balenciagatrack.svg"},
+
+    {id: 5, title: "Nike Blazer Mid x Sacai", colorway: "Snow Beach Colorway", brand: "nike", collab: "sacai",
+    type: "high", reviews: 39, colorPrimary: "yellow", colorSecondary: "blue", stock: 2, price: 140, imgsrc: "img/blazersacai.svg"},
+
+    {id: 6, title: "Dior B23 High-Top", colorway: "Black / White Colorway", brand: "dior", collab: null, 
+    type: "high", reviews: 51, colorPrimary: "white", colorSecondary: "black", stock: 4, price: 350, imgsrc: "img/diorb23.svg"},
+
+    {id: 7, title: "New Balance 990v3 x Stray Rats", colorway: "The Joker Colorway", brand: "newbalance", collab: null,
+    type: "low", reviews: 32, colorPrimary: "green", colorSecondary: "purple", stock: 2, price: 160, imgsrc: "img/nb990.svg"},
+
+    {id: 8, title: "New Balance 2002R x Salehe Bembury", colorway: "Peace Be The Journey Colorway", brand: "newbalance", collab: null, 
+    type: "low", reviews: 39, colorPrimary: "orange", colorSecondary: null, stock: 3, price: 125, imgsrc: "img/nb2002R.svg"},
+
+    {id: 9, title: "Nike Air Zoom Alphafly Next%", colorway: "Racer Blue / Pink Glow Colorway", brand: "nike", collab: null, 
+    type: "low", reviews: 21, colorPrimary: "black", colorSecondary: "green", stock: 1, price: 220, imgsrc: "img/nikealphafly.svg"},
+
+    {id: 10, title: "Nike Adapt BB Mag", colorway: "Wolf Gray Colorway", brand: "nike", collab: null,
+    type: "low", reviews: 63, colorPrimary: "grey", colorSecondary: "blue", stock: 4, price: 300, imgsrc: "img/nikebb.svg"},
+
+    {id: 11, title: "Jordan 1 Retro High x OFF-WHITE", colorway: "Chicago Colorway", brand: "nike", collab: "offwhite", 
+    type: "high", reviews: 58, colorPrimary: "white", colorSecondary: "red", stock: 3, price: 190, imgsrc: "img/offjordan1.svg"},
+
+    {id: 12, title: "Nike Dunk Low x OFF-WHITE", colorway: "Pine Green Colorway", brand: "nike", collab: "offwhite", 
+    type: "low", reviews: 52, colorPrimary: "green", colorSecondary: "orange", stock: 3, "price": 180, imgsrc: "img/offdunklow.svg"},
+
+    {id: 13, title: "Nike Zoom Tempo NEXT% x OFF-WHITE", colorway: "Racer Blue / Pink Glow Colorway", brand: "nike", collab: "offwhite",
+    type: "low", reviews: 12, colorPrimary: "pink", colorSecondary: "purple", stock: 1, price: 260, imgsrc: "img/offnikezoom.svg"},
+
+    {id: 14, title: "OFF-WHITE Odsy-2000 SS21", colorway: "Mint Blue Colorway", brand: "offwhite", collab: null, 
+    type: "low", reviews: 55, colorPrimary: "blue", colorSecondary: null, stock: 2, price: 275, imgsrc: "img/offodsy2000.svg"},
+
+    {id: 15, title: "Nike Vaporwaffle x Sacai", colorway: "Villain Red Neptune Green Colorway", brand: "nike", collab: "sacai", 
+    type: "low", reviews: 40, colorPrimary: "burgundy", colorSecondary: "green", stock: 5, price: 180, imgsrc: "img/wafflesacai.svg"},
+
+    {id: 16, title: "Adidas Yeezy 500", colorway: "Stone Colorway", brand: "adidas", collab: "yeezy",
+    type: "low", reviews: 90, colorPrimary: "beige", colorSecondary: null, stock: 4, price: 200, imgsrc: "img/yeezy500.svg"},
+
+    {id: 17, title: "Adidas Yeezy Boost 350 V2", colorway: "Core Black / White Colorway", brand: "adidas", collab: "yeezy", 
+    type: "low", reviews: 35, colorPrimary: "black", colorSecondary: "white", stock: 2, "price": 220, imgsrc: "img/yeezy350.svg"},
+
+    {id: 18, title: "Adidas Yeezy 700 V3", colorway: "Arzareth Colorway", brand: "adidas", collab: "yeezy", 
+    type: "low", reviews: 73, colorPrimary: "blue", colorSecondary: "white", stock: 3, price: 250, imgsrc: "img/yeezy700.svg"}
+]
--- JavaScript/proyectoFinal/js/main.js
+++ JavaScript/proyectoFinal/js/main.js
@@ -3,6 +3,31 @@ THIS FILE CONTAINS ALL THE LOGIC FOR THE FLOW OF OUR WEB APP.
 it consumes variables.js, data.js and functions.js.
 ----------------------------------------------------------------------------------------------------------*/
 
+/*----------------------------------------------------------------------------------------------------------
+Dynamic products' grid
+----------------------------------------------------------------------------------------------------------*/
+for (let i = 0; i <storeItems.length; i++) {
+    let container = document.createElement("div");
+    container.innerHTML =  
+      `<a href="#" class="card-hyperlink">
+        <div class="card">
+          <img src="${storeItems[i].imgsrc}" class="item-img" alt="">
+          <h1 class="name">${storeItems[i].title}</h1>
+          <h3 class="colorway">${storeItems[i].colorway}</h3>
+          <input type="image" src="img/in-wishlist.svg" class="itemcontainer add-wishlist"/>
+          <h2 class="price itemcontainer">$ ${storeItems[i].price}</h2>
+          <input type="image" src="img/in-bag.svg" class="itemcontainer add-bag" id="11"/>
+          <p class="reviews"> ${storeItems[i].reviews}🔥 </p>
+        </div>
+      </a>`;
+    let parentNode = document.getElementById("product-grid");
+    parentNode.appendChild(container);
+  };
+
+
+/*----------------------------------------------------------------------------------------------------------
+Flow of web app
+----------------------------------------------------------------------------------------------------------*/
 do {
     itemId = mainMenu()
     if (itemId === (storeItems.length + 1)) break;
@@ -19,7 +44,6 @@ while (answer == "S")
 if (bag > 0) {
     alert(`El total de su compra es de ${bag}`)
 }
-
 
 else {
     alert("Gracias por su visita")
