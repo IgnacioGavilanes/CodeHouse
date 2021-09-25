b8d12901b643b5d68d65c4e9d94a1ec3c16de2ba
Ignacio Gavilanes <77927772+IgnacioGavilanes@users.noreply.github.com>
2021-09-25 17:34:00 -0300

    pre 3ra entrega

--- JavaScript/proyectoFinal/js/main.js
+++ JavaScript/proyectoFinal/js/main.js
@@ -24,13 +24,19 @@ for (let i = 0; i <storeItems.length; i++) {
 
 let bagButton = $(".add-bag"); 
 
-for (let i = 0; i < bagButton.length; i++) {
+//Number of items in bag
+let bagItems = 0
 
+for (let i = 0; i < bagButton.length; i++) {
+  
   $(bagButton[i]).on('click', addToBagEvent);
   function addToBagEvent (event) {
+     
     // if (checkStock()) {
+    // bagItems = bagItems + 1;
     let bagButtonClicked = event.target
     let addItem = bagButtonClicked.parentElement
+
 
     //NO PUDE LOGRAR UTILIZAR SELECTORES JQUERY PARA RECREAR ESTA ACCION  
     let itemPrice = addItem.getElementsByClassName('price')[0].innerText
@@ -40,11 +46,18 @@ for (let i = 0; i < bagButton.length; i++) {
 
     console.log(itemBagArray)
 
+    bagCounter = parseInt(itemBagArray.length);
+    $("#counter").empty().append(bagCounter)
+    // console.log(bagItems)
+    console.log(bagCounter)
+
+
+
     localStorage.setItem(JSON.stringify(itemName), JSON.stringify(itemPrice))
 
-    //Item added to card notification
-    Swal.fire(
-      {  position: 'top-end',
+    //Item added to  notification
+    Swal.fire({
+      position: 'top-end',
       toast:true,
       icon: 'success',
       background: 'white',
@@ -79,7 +92,7 @@ $(".reviews").click(function () {
     this.style.fontWeight = "bold";
     
     //TODAVIA NO SE COMO HACER QUE CAMBIE EL NUMERO DEL ELEMENTO QUE ESTOY CLICKEANDO Y NO TODOS LOS QUE TIENEN LA
-    //CLASE REVIEW. TAMPOCO SE PORQUE EL PRIMER CLICK VA AL ELSE, CUANDO EL COLOR PREDETERMINADO DEL OBJETO QUE ESTOY CLICKEANDO
+    //CLASE REVIEWS. TAMPOCO SE PORQUE EL PRIMER CLICK VA AL ELSE, CUANDO EL COLOR PREDETERMINADO DEL OBJETO QUE ESTOY CLICKEANDO
     //ES NEGRO...
 
     function addReview () {
@@ -98,10 +111,131 @@ $(".reviews").click(function () {
       console.log(reviewNum);
       $(".reviews").html(reviewNum - 1);
     }
-
     subtractReview();
   }
 });
+
+
+
+// $(".filters-title").click(() => {
+//   $(".sidebar").toggle(500, function(){
+//     $(".filters-title").css({
+//         "width": "15rem",
+//       });
+//     $(".sidebar").css({
+//       "display":"inline-block",
+
+//     })
+//     })
+//   });
+
+
+// $(document).ready(function(){
+//     $(".filters-title").click(function(){
+//         $(".sidebar").toggle(500, function(){
+//           $(".filters").css({
+//             "margin-top":"4rem",
+//           })
+//           $(".filters-title").css({
+//             "display":"none",
+//           })
+//           $(".collapse-filters").css({
+//             "display":"block",
+//           })
+//         });
+//       })
+// });
+
+//FUNCIONA
+// $(".filters-title").click(function(){
+//   $(".sidebar").fadeIn(100, function(){
+//     $(".filters").css({
+//       "margin-top":"4rem",
+//     })
+//     $(".filters-title").css({
+//       "display":"none",
+//     })
+//     $(".collapse-filters").css({
+//       "display":"block",
+//   })
+// })})
+
+$(".filterbtn-container").click(function(){
+  $(".sidebar").slideToggle(500)
+  $(".filterbtn-container").css({
+    "width":"15rem"
+  })
+})
+
+
+
+//BAG
+//PARA PODER MOSTRAR LOS PRODUCTOS DEL CARRITO EN SWEETALERT VOY 
+//A TENER QUE CREAR UNA VARIABLE VACIA 'HTMLBAG' EN LA CUAL TENGO 
+//UN LOOP CON TODOS LOS ELEMENTOS DEL ARRAY QUE AGREGUE, Y AHI PASAR
+//ESTO COMO UN PARAMETRO HMTL DENTRO DE SWAL.FIRE.
+//DENTRO DEL HTMLBAG TENGO QUE AGREGAT CLASES PARA AGREGAR ESTILOS.
+//TAMBIEN AGREGAR <BR>. ADEMAS VER QUE SI AGREGO EL MISMO PRODUCTO 2
+//VECES NO SE DUPLIQUE TODO EL HTML PERO SE INCREMENTE LA CANTIDAD
+
+$("#bag").click(function(){
+  Swal.fire({
+    title: 'Your Bag',
+    text: 'Do you want to continue',
+    width: 800,
+    showLoaderOnConfirm: true,
+    confirmButtonText: 'Go To Payment Method',
+    showCloseButton: true,
+    confirmButtonColor: '#3085d6',
+    html: '<hr> <br>'+
+      '<img src="img/yeezy350.svg" width=150rem><br>'+
+      '<img src="img/nb990.svg" width=150rem><br>'+
+      '<img src="img/nikebb.svg" width=150rem><br>'+
+      '<img src="img/diorb23.svg" width=150rem><br>'+
+      '<img src="img/yeezy350.svg" width=150rem><br>'+
+      '<img src="img/nb990.svg" width=150rem><br>'+
+      '<img src="img/nikebb.svg" width=150rem><br>',
+    //Need to add item name, img, price, qtty, total
+  }).then(function (result) {
+    if (result.isConfirmed) {
+      window.location = "checkout.html"
+    }
+  })
+},)
+
+//WISHLIST
+$("#wishlist").click(function(){
+  Swal.fire({
+    html: '<hr>',
+    title: 'Your Wishlist',
+    width: 800,
+    showLoaderOnConfirm: true,
+    showCloseButton: true,
+  })})
+
+
+
+//FUNCIONA
+// $(".arrow-left").click(function(){
+//   $(".sidebar").fadeOut(100, function(){
+//     $(".filters").css({
+//       "margin-top":"4rem",
+//     })
+//     $(".filters-title").css({
+//       "display":"block",
+//     })
+//     $(".collapse-filters").css({
+//       "display":"none",
+//   })
+//   $(".arrow-left").css({
+//     "transform":"rotate(180deg)",
+//     "transition": "all 3s",
+//     "transition-timing": "ease-in-out",
+//     "width":"3rem",
+//     "height":"auto",
+//   })
+// })})
+
 
 
 //function checkStock () {
@@ -126,3 +260,12 @@ $(".reviews").click(function () {
   //     return true
   // }
 //}
+
+function responseJSON (state) {
+  if (state === "success"){
+    console.log("success")
+  }
+}
+
+
+$.ajax({url:"js/data.json", datatype:"json", success: responseJSON})
